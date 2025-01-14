1ac4528bf3498d3308f0a202854d3b38a76f501d
Stuart Forrester <stforrester@gmail.com>
2025-01-14 15:31:25 -0600

    completes the lab and passes all tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 15, 'M', 'Dark Chocolate Brown', 'Friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 25, 'F', 'Red', 'Mischevious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 35, 'F', 'Blonde', 'Happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 40, 'M', 'Black', 'Stoic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 18, 'F', 'Light Brown', 'Gregarious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'Green', 'Angsty', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 17, 'F', 'Orange', 'Hungry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, 'M', 'White', 'Cuddly', 1);
